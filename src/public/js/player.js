a42b940369baf3679b4adf8537e24665021daba1
Roman Vasilevich <r.vasilevich@vk.team>
2025-02-15 11:45:00 +0300

    Добавил множественную сортировку графики.

--- src/public/js/player.js
+++ src/public/js/player.js
@@ -5,6 +5,8 @@ const renderPlayers = () => {
     search: true,
     sortName: 'rating',
     sortOrder: 'desc',
+    showMultiSort: true,
+    showMultiSortButton: true,
     /*pagination: true, pageSize: 20, pageNumber: 1,*/
     showRefresh: true,
     columns: [
@@ -134,6 +136,47 @@ const renderPlayerModal = (player) => {
     $("#player-window").html($(playerModalTemplate(player)));
 
     let modal = new bootstrap.Modal(document.getElementById('player-window'));
+
+    new Chart(document.getElementById("line-chart"), {
+      type: 'line',
+      data: {
+        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        datasets: [{
+          data: [86,114,106,106,107,111,133,221,783,2478],
+          label: "Africa",
+          borderColor: "#3e95cd",
+          fill: false
+        }, {
+          data: [282,350,411,502,635,809,947,1402,3700,5267],
+          label: "Asia",
+          borderColor: "#8e5ea2",
+          fill: false
+        }, {
+          data: [168,170,178,190,203,276,408,547,675,734],
+          label: "Europe",
+          borderColor: "#3cba9f",
+          fill: false
+        }, {
+          data: [40,20,10,16,24,38,74,167,508,784],
+          label: "Latin America",
+          borderColor: "#e8c3b9",
+          fill: false
+        }, {
+          data: [6,3,2,2,7,26,82,172,312,433],
+          label: "North America",
+          borderColor: "#c45850",
+          fill: false
+        }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'World population per region (in millions)'
+        }
+      }
+    });
+
     modal.show();
   });
 }
@@ -155,6 +198,7 @@ const playerModalTemplate = (player) => `
       </div>
       <label id="player-window-label-birthday">${player['birthday']} (${player['age']})</label>
     </div>
+    <canvas id="line-chart" width="800" height="450"></canvas>
     <div class="modal-footer">
     </div>
   </div>
