23a2d6f9d2964257f2c1cd628695f8ed73ea7ec2
Roman Vasilevich <r.vasilevich@vk.team>
2025-02-22 22:31:15 +0300

    Команда для разворачивания релиза.

---
+++ etc/scripts/release.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo ln -snf /home/r/badmr/etc/systemd/system/badmr.service /etc/systemd/system/badmr.service
+sudo systemctl daemon-reload
+sudo systemctl enable badmr.service
+
+sudo systemctl stop badmr.service
+rm -f /home/r/badmr/badmr.sqlite3
+sudo systemctl start badmr.service
